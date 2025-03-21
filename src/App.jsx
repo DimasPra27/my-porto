d71810312b3e627658cbe63b0f58e54751107146
DimasPra27Ids <dimas.prasetyo@idstar.group>
2025-03-21 13:38:31 +0700

    update typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Contact from "./sections/Contact/Contact";
 import Footer from "./sections/Footer/Footer";
-import Hero from "./sections/hero/Hero";
+import Hero from "./sections/Hero/Hero";
 import Projects from "./sections/Projects/Projects";
 import Skills from "./sections/Skills/Skills";
 import Experience from "./sections/Experience/Experience";
